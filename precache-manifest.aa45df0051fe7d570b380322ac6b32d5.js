505e2479225cf49db385d327915aaca5c7aa31ca
Andrew Nickyforov <nik_andrey@ukr.net>
2020-06-22 22:41:57 +0300

    Updates

---
+++ precache-manifest.aa45df0051fe7d570b380322ac6b32d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd6134b5ecc0ac2b568d4d19c213fee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "550bae07f62f03746649",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c40b73c1e8c4b77a5cee",
+    "url": "./static/js/2.d383e718.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d383e718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550bae07f62f03746649",
+    "url": "./static/js/main.2d1e71b1.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
